e374154d06208679c05409dba5e8a1589eea10a5
tucker isaacs <isaacstb@mail.uc.edu>
2022-09-15 14:53:08 -0400

    task 1 lab 4

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double salePrice=35.5;
+        double saleTax=.05;
+
+        double totalPrice=salePrice*saleTax;
+        System.out.println("the sales tax is "+totalPrice);
+    }
+}
